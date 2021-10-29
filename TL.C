0ec1f67b2ceef435f117c6644a13726ecddd7404
Jing LIU <jing.liu@usd.edu>
2021-10-29 00:09:11 -0500

    added e data

--- TL.C
+++ TL.C
@@ -4,10 +4,16 @@
 
 	// data, hole, Reggiani, PRB 16 (1977) 2781 
 	const int n = 7;
-	float vs100[n] = {1.21, 1.20, 1.16, 1.13, 0.98, 0.82, 0.76};
-	float dvs[n] = {0.057, 0.059, 0.046, 0.052, 0.048, 0.034, 0.036};
-	float tl[n] = {8, 20, 40, 77, 130, 190, 220}, dt[n]={0};
-	TGraphErrors *gh = new TGraphErrors(n, tl, vs100, dt, dvs);
+	float vh100[n] = {1.21, 1.20, 1.16, 1.13, 0.98, 0.82, 0.76};
+	float dvh[n] = {0.057, 0.059, 0.046, 0.052, 0.048, 0.034, 0.036};
+	float th[n] = {8, 20, 40, 77, 130, 190, 220}, dt[n]={0};
+	TGraphErrors *gh = new TGraphErrors(n, th, vh100, dt, dvh);
+
+	// data, electron, Jacoboni, PRB 24 (1981) 1014
+	float ve100[n+1] = {1.9, 1.5, 1.2, 1.1, 1.05, 0.9, 0.82, 0.62};
+	float dve[n+1] = {0.057, 0.059, 0.046, 0.052, 0.048, 0.034, 0.036, 0.036};
+	float te[n+1] = {8, 20, 45, 77, 130, 190, 240, 300};
+	TGraphErrors *ge = new TGraphErrors(n+1, te, ve100, dt, dve);
 
 	// model, Quay, Mat. Sci. Semi. Proc. 3 (2000) 149
 	TF1 *f = new TF1 ("f", "[0]/(1-[1]+[1]*x/300)", 0, 330);
@@ -17,16 +23,18 @@
 	TF1 *fh = (TF1*) f->Clone("fh");
 
 	// draw
-	gh->GetXaxis()->SetRangeUser(0,290);
-	gh->GetYaxis()->SetRangeUser(0.65,1.35);
-	gh->SetTitle(";Temperature [K]; V_{sat} [10^{7} cm/s]");
-	gh->SetMarkerStyle(20);
-	gh->Draw("ap");
+	ge->GetXaxis()->SetRangeUser(0,320);
+	ge->GetYaxis()->SetRangeUser(0.55,2.05);
+	ge->SetTitle(";Temperature [K]; V_{sat} along #LT 100 #GT [10^{7} cm/s]");
+	ge->SetMarkerStyle(20);
+	ge->Draw("ap");
+	gh->SetMarkerStyle(21);
+	gh->Draw("p");
 
 	fe->SetLineColor(kRed);
 	fh->SetLineColor(kBlue);
 
-	fe->SetParameters(0.72, 0.45);
+	fe->SetParameters(0.70, 0.45);
 	fh->SetParameters(0.63, 0.39);
 
 	fe->Draw("same");
@@ -34,7 +42,8 @@
 
 	TLegend *l = new TLegend(0.6, 0.7, 0.98, 0.98);
 	l->AddEntry(gh, "Hole (data)", "p");
-	l->AddEntry(fe, "Electron (model)", "l");
+	l->AddEntry(ge, "Electron (data)", "p");
 	l->AddEntry(fh, "Hole (model)", "l");
+	l->AddEntry(fe, "Electron (model)", "l");
 	l->Draw();
 }
